3774211736f574e689242f0b35f0960ab293ec28
a.cooperrider <coopdh@gmail.com>
2022-09-28 15:17:19 -0500

    form filling

--- Website/auth.py
+++ Website/auth.py
@@ -1,15 +1,17 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
+    data = request.form
+    print(data)
     return render_template("login.html")
 
 @auth.route('/logout')
 def logout():
     return "<p>Logout</p>"
 
-@auth.route('/adduser')
+@auth.route('/adduser', methods=['GET', 'POST'])
 def adduser():
-    return "<p>add user</p>"
+    return render_template("adduser.html")
