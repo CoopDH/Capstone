f104db3907c3e7d07a5a240fac450cea099a2387
a.cooperrider <coopdh@gmail.com>
2022-09-28 15:55:19 -0500

    For Korivain

--- Website/auth.py
+++ Website/auth.py
@@ -1,11 +1,11 @@
+from unicodedata import category
 from flask import Blueprint, render_template, request
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    data = request.form
-    print(data)
+
     return render_template("login.html")
 
 @auth.route('/logout')
@@ -14,4 +14,29 @@ def logout():
 
 @auth.route('/adduser', methods=['GET', 'POST'])
 def adduser():
+    #injests from the form being filled out
+    if request.method == 'POST':
+        fname = request.form.get('fname')
+        lname = request.form.get('lname')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+        user_status = request.form.get('user_status') #will return 'on' if enabled
+        adminpassword = request.form.get('adminpassword')
+
+        #Entry validation
+        if len(fname) < 2:
+            flash('First name needs to be larger than 1 character.', category='error')
+        elif len(lname) < 2:
+            flash('Last name needs to be larger than 1 character.', category='error')
+        elif password1 != password2:
+            flash('Passwords need to match.', category='error')
+        elif len(password1) < 7:
+            flash('Password needs to be 7 characters or longer.', category='error')
+        #elif adminpassword != current user password
+        else:
+            flash('User added.', category='success')
+            #create username
+            #add user to database
+
+
     return render_template("adduser.html")
--- Website/views.py
+++ Website/views.py
@@ -3,6 +3,6 @@ from flask import Blueprint, render_template
 views = Blueprint('views', __name__)
 
 #defineing the home page route and following pages
-@views.route('/home')
+@views.route('/')
 def home():
     return render_template("home.html")
